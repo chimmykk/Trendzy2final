536832103dd70d5df4aebc9d1dd2f0b430f5871e
sem22-dev <thotjj22@gmail.com>
2023-10-04 21:56:56 +0530

    server side error

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -6,9 +6,7 @@ import SettingOptions from "./settingComponents/settingOptions";
 const getobjectId = async () => {
   const session = await getServerSession(authOptions);
   const email = session?.user?.email || "Email not found"; 
-  const res = await fetch(`http://localhost:3000/ api/fetch/route?email=${email}`, {
-    next: {revalidate: 30}
-  })
+  const res = await fetch(`http://localhost:3000/ api/fetch/route?email=${email}`)
   const data = await res.json();
   return data._id
 }
@@ -16,9 +14,7 @@ const getobjectId = async () => {
 const getSellerInfo = async () => {
   // const ObjectData = await getobjectId();
   const objectId = await getobjectId()
-  const res = await fetch(`http://localhost:3000/ api/fetch/fetchaddress?objectId=${objectId}`, {
-    next: {revalidate: 30}
-  });
+  const res = await fetch(`http://localhost:3000/ api/fetch/fetchaddress?objectId=${objectId}`);
   const data = await res.json();
   return data;
 }
@@ -26,9 +22,7 @@ const getSellerInfo = async () => {
 const fetchProfileImage = async () => {
   const session = await getServerSession(authOptions);
   const email = session?.user?.email || "Email not found"; 
-  const res = await fetch(`http://localhost:3000/ api/upload/image?email=${email}`, {
-    next: {revalidate: 30}
-  })
+  const res = await fetch(`http://localhost:3000/ api/upload/image?email=${email}`)
   const data = await res.json();
   return data;
 }
@@ -36,9 +30,7 @@ const fetchProfileImage = async () => {
 const fetchProfileBanner = async () => {
   const session = await getServerSession(authOptions);
   const email = session?.user?.email || "Email not found"; 
-  const res = await fetch(`http://localhost:3000/ api/upload/banner?email=${encodeURIComponent(email)}`, {
-    next: {revalidate: 30}
-  })
+  const res = await fetch(`http://localhost:3000/ api/upload/banner?email=${encodeURIComponent(email)}`)
   const data = await res.json();
   return data;
 }
