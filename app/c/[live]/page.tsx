bc9944cff12004a6942ab40710c041f507be5555
sem22-dev <thotjj22@gmail.com>
2023-10-09 15:22:13 +0530

    https://trendzy2.vercel.app/api/flow/postget

--- app/c/[live]/page.tsx
+++ app/c/[live]/page.tsx
@@ -8,29 +8,29 @@ interface UserLive {
 }
 
 
-export async function generateStaticParams() {
-  const response = await fetch('https://trendzy2.vercel.app/api/flow/postget');
-  const data = await response.json();
+// export async function generateStaticParams() {
+//   const response = await fetch('https://trendzy2.vercel.app/api/flow/postget');
+//   const data = await response.json();
 
-  return data.data.map((item: UserLive[]) => ({
-    live: item[0]?.userlive.channelName.toString()
-  }));
-}
+//   return data.data.map((item: UserLive[]) => ({
+//     live: item[0]?.userlive.channelName.toString()
+//   }));
+// }
 
 
-interface StreamParams {
-  params: {
-    live: string;
-  };
-}
+// interface StreamParams {
+//   params: {
+//     live: string;
+//   };
+// }
 
-export default async function Stream({ params }: StreamParams) {
-  const { live } = params
+export default async function Stream() {
+
   
   return (
     <div className="pl-64">
-      <h1>hi {live}</h1>
-      <Audience channelName={live}/>
+      <h1>hi </h1>
+      <Audience channelName={"qwerty"}/>
     </div>
   )
 }
