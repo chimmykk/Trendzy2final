f1a3d593d8c5a331f23f3729b7905d45bc1079b6
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2023-11-01 23:36:22 +0530

    fixed

--- app/LiveRoom/channel/Chat.tsx
+++ app/LiveRoom/channel/Chat.tsx
@@ -4,7 +4,7 @@ export default function Chat() {
 
   return (
     <div className="flex flex-col fixed  right-0 px-2 text-white w-[300px] py-4 h-full">
-      hi there
+      Live Chat
     </div>
   );
 }
